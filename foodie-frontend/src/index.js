828e705ace1fe1f23d05f95eff31c912fa7fb53f
Andres Maza <amaza.ag@gmail.com>
2017-04-04 16:23:43 -0400

    Add require favicon to be added to /dist

--- foodie-frontend/src/index.js
+++ foodie-frontend/src/index.js
@@ -5,6 +5,10 @@ import { Route, Router, browserHistory } from 'react-router';
 import './styles/normalize.css';
 import './styles/style.css';
 
+// Require favicon image for build on 'npm run build'
+import './assets/favicon-32x32.png';
+import './assets/favicon-16x16.png';
+
 import Homepage from './components/Homepage/Homepage';
 import SearchResults from './components/SearchResults/SearchResults';
 
